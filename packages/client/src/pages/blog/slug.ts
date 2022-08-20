7a18ec6fb492345afff180a48fec176847fad420
Teemu Koivisto <tkoivisto456@gmail.com>
2022-08-20 15:19:25 +0300

    fix: blog/index.html tags

--- packages/client/src/pages/blog/slug.ts
+++ packages/client/src/pages/blog/slug.ts
@@ -1,5 +1,8 @@
 import '../../index.css'
+import './slug.css'
 
 import '$components/nav-bar'
 import '$components/my-footer'
+import '$components/blog-header'
+import '$components/blog-pager'
 import '$components/post-tags'
