d016f839327fb2ebf70748cffeddc4136462f6f1
Teemu Koivisto <tkoivisto456@gmail.com>
2022-08-28 00:19:06 +0300

    fix: use BASE_URL in component html

--- packages/client/src/utils/html.ts
+++ packages/client/src/utils/html.ts
@@ -1,5 +1,13 @@
+const { BASE_URL } = import.meta.env
+
 export function htmlToElement(html: string) {
+  // trim() -> Never return a text node of whitespace as the result
+  const formatted = html
+    .trim()
+    .replaceAll('src="/', `src="${BASE_URL}`)
+    .replaceAll("@import url('/", `@import url('${BASE_URL}`)
+    .replaceAll('href="/', `href="${BASE_URL}`)
   const template = document.createElement('template')
-  template.innerHTML = html.trim() // Never return a text node of whitespace as the result
+  template.innerHTML = formatted
   return template.content
 }
