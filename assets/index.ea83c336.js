3eb88919ef9478c2b878fa1e89f7146031afbdcc
TeemuKoivisto <TeemuKoivisto@users.noreply.github.com>
2022-11-05 21:11:34 +0000

    deploy: 806a2d4440446c48f8e23f6277bb9623b5193a6b

--- assets/index.ea83c336.js
+++
@@ -1,5 +0,0 @@
-import{h as l}from"./index.10c09ab4.js";const n=`<style>
-  @import url('/index.css');
-</style>
-<ul class="flex flex-wrap"></ul>
-`;customElements.define("post-tags",class extends HTMLElement{constructor(){super(),this.postTags=[];const t=l(n);this.attachShadow({mode:"open"}).appendChild(t.cloneNode(!0))}connectedCallback(){setTimeout(()=>{const t=this.attributes.getNamedItem("data-post-tags");t==null||t.value.split(",").forEach(e=>{this.postTags.push(e)}),this.render()})}render(){var e;const t=(e=this.shadowRoot)==null?void 0:e.querySelector("ul");t&&(t.innerHTML=this.postTags.reduce((s,o)=>s+=`<li class="px-2 mb-1 mr-1 text-sm text-white bg-red-400 rounded-md leading-6 text-base:xsm mb-2:xsm mr-2:xsm">${o}</li>`,""))}});
--- assets/main.88d879b9.js
+++
@@ -1,1 +0,0 @@
-import"./index.10c09ab4.js";import"./.a6f5181c.js";
