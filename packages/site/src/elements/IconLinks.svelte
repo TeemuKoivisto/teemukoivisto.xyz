eb86911ee4a98687372761750592f1cd2a6f6f76
Teemu Koivisto <tkoivisto456@gmail.com>
2024-01-13 17:30:10 +0200

    add image alt, width, height and meta theme-color since lighthouse complains

--- packages/site/src/elements/IconLinks.svelte
+++ packages/site/src/elements/IconLinks.svelte
@@ -5,21 +5,27 @@
       href="mailto:teemukoivisto.xyz@gmail.com"
       aria-label="Send an email"
     >
-      <img src="/svg/mail.svg" style="filter: invert(1)" />
+      <img src="/svg/mail.svg" style="filter: invert(1)" alt="Mail" width={24} height={24} />
     </a>
     <a
       class="flex mr-2 text-white cursor-pointer"
       href="https://github.com/teemukoivisto"
       aria-label="My Github profile"
     >
-      <img src="/svg/github.svg" style="filter: invert(1)" />
+      <img src="/svg/github.svg" style="filter: invert(1)" alt="GitHub" width={24} height={24} />
     </a>
     <a
       class="flex mr-2 text-white cursor-pointer"
       href="https://www.linkedin.com/in/teemu-koivisto-75304b114"
       aria-label="My Linkedin profile"
     >
-      <img src="/svg/linkedin.svg" style="filter: invert(1)" />
+      <img
+        src="/svg/linkedin.svg"
+        style="filter: invert(1)"
+        alt="LinkedIn"
+        width={24}
+        height={24}
+      />
     </a>
   </div>
   <address class="p-0 m-0 text-sm not-italic text-white">teemukoivisto.xyz ~АТ~ gmail.com</address>
