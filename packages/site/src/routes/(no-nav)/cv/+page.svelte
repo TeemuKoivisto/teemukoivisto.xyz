9d0b69429808a0fd9ff302a88cb04e69062bbb30
Teemu Koivisto <tkoivisto456@gmail.com>
2024-02-13 13:21:58 +0200

    update cv

--- packages/site/src/routes/(no-nav)/cv/+page.svelte
+++ packages/site/src/routes/(no-nav)/cv/+page.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte/dist/OfflineIcon.svelte'
+  import location from '@iconify-icons/mdi/location'
+
   import { CV_EMAIL, CV_PHONE } from '$config'
+
+  const phone = CV_PHONE || ''
+  const email = CV_EMAIL || 'teemukoivisto.xyz@gmail.com'
 </script>
 
 <div class="page px-4">
   <div class="flex py-2 border-b-2 justify-between">
-    <a href="https://teemukoivisto.xyz">https://teemukoivisto.xyz</a>
+    <a href="https://teemukoivisto.xyz/cv">https://teemukoivisto.xyz/cv</a>
     <div>
-      <span class="font-bold"> {CV_PHONE} </span>
-      <a href={`mailto:${CV_EMAIL}`} aria-label="Send an email">
-        {CV_EMAIL}
+      <span class="font-bold"> {phone} </span>
+      <a href={`mailto:${email}`} aria-label="Send an email">
+        {email}
       </a>
     </div>
   </div>
-  <div class="mt-6">
+  <div class="">
     <header>
       <figure class="avatar flex items-center justify-center">
         <img
@@ -24,8 +30,12 @@
         />
       </figure>
       <div class="ml-1 flex flex-col">
-        <h1 class="text-5xl font-sans tracking-tight">Teemu Koivisto — CV</h1>
-        <div class="my-2 ml-1 flex">
+        <h1 class="ml-[-16px] mt-4 text-5xl font-sans tracking-tight">Teemu Koivisto — CV</h1>
+        <div class="my-2 flex">
+          <div class="mr-4 flex items-center">
+            <Icon icon={location} width={24} />
+            <span class="ml-1">Helsinki</span>
+          </div>
           <a
             class="flex mr-2 cursor-pointer"
             href="https://github.com/teemukoivisto"
@@ -42,15 +52,15 @@
           </a>
         </div>
         <blockquote class="ml-1 italic">
-          Data scientist by degree but craftsman by blood. I rather full-ass than half-ass but also
-          know when good is not only good but good enough. Enjoys all parts of the stack from
-          writing infra to frontend widgets although rich-text is where I really shine. Loves good
-          team effort, dislikes ego-driven programming (or anything). Always seeking new interesting
+          Data scientist by degree but craftsman by blood. I rather full-ass than half-ass but have
+          learnt that sometimes good is just not good but good enough. Enjoys all parts of the stack
+          from writing infra to frontend widgets although rich-text is my jam. Loves good team
+          effort, dislikes ego-driven programming (or anything). Always seeking new interesting
           challenges.
         </blockquote>
       </div>
     </header>
-    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4 text-sm">
+    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4">
       <legend class="text-base px-1"> Skills </legend>
       <ul>
         <li>
@@ -66,51 +76,111 @@
           <p class="ml-1">
             AWS, Terraform, Ansible, Cloudflare, Vue, SQL, Python, data engineering, Jupyter, Rust*
           </p>
-          <small class="ml-1">[*] Probably still within Dunning-Kruger effect</small>
+          <small>[*] Probably still within Dunning-Kruger effect</small>
         </li>
       </ul>
     </fieldset>
-    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4 text-sm">
+    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4">
       <legend class="text-base px-1"> Experience </legend>
       <ul>
         <li>
           <h3 class="text-xl font-sans flex">
             <span class="year"> 2023 </span>
-            <span> Focal Oy </span>
+            <a href="https://focal.inc/">Focal Oy — Full-stack developer</a>
           </h3>
-          <p>Things</p>
+          <p>
+            I was contacted by Tuomo, CEO of Focal, as they had seen my ProseMirror contributions. I
+            then joined as rich-text expert to work on their Notion-type product for marketing
+            pipelines although I ended up doing mostly regular Vue/NodeJS work. Really cool team and
+            product but sadly things just didn't align. JS/TS, ProseMirror, Vue, NodeJS, Yjs,
+            Postgres, GCP.
+          </p>
         </li>
         <li>
           <h3 class="text-xl font-sans flex">
             <span class="year">2022</span>
-            <span>Skiff Inc</span>
+            <a href="https://skiff.com/">Skiff Inc — Frontend developer (contract)</a>
           </h3>
-          <p>Things</p>
+          <p>
+            After Atypon, I sought another ProseMirror gig and saw Skiff was hiring contractors.
+            While I did some interesting work with their rich-text editor, it wasn't necessarily the
+            best match with me alone in Finland and the majority of the team in Silicon Valley. TS,
+            ProseMirror, React.
+          </p>
         </li>
         <li>
           <h3 class="text-xl font-sans flex">
             <span class="year">2020 - 2022</span>
-            <span>Atypon Ltd</span>
+            <a href="https://www.atypon.com/">Atypon Ltd — Frontend developer (contract)</a>
           </h3>
-          <p>Things</p>
+          <p>
+            For my first job after graduation I really wanted to dive deep into rich-text editing
+            with ProseMirror. I then became part of Atypon's Manuscripts team with special task of
+            enhancing the rich-text editor and its integrations. After the start, I started
+            rewriting the previous track-changes functionality on my own which I succeeded after a
+            lot of pain and trouble. TS, ProseMirror, React, NodeJS, Yjs.
+          </p>
         </li>
         <li>
           <h3 class="text-xl font-sans flex">
             <span class="year">2020</span>
-            <span>Aalto University</span>
+            <a href="https://www.aalto.fi/">Aalto University — Thesis worker</a>
           </h3>
-          <p>Things</p>
+          <p>
+            For my thesis, I asked my old lecturer (Arto Hellas) for a project and was paid by his
+            research group <a href="https://research.cs.aalto.fi/LeTech/">(LeTech)</a> to work on
+            it. The project was this prototype app
+            <a href="https://github.com/Aalto-LeTech/CodeClusters">(CodeClusters)</a>
+            to cluster program code by their ASTs to help teachers/TAs to analyze/grade them faster.
+            TS, Python, clustering, information retrieval, Solr, React, NodeJS, Postgres.
+          </p>
         </li>
         <li>
           <h3 class="text-xl font-sans flex">
             <span class="year">2017 - 2020</span>
-            <span>Webscale Oy</span>
+            <a href="https://www.webscale.fi/">Webscale Oy — Full-stack developer</a>
           </h3>
-          <p>Things</p>
+          <p>
+            In my first proper programming job I learnt the mindset of what it takes to be a good
+            developer. This involved getting very familiar with AWS, TypeScript, NodeJS as well as
+            React and learning how to build as a team services such as Yle's and KSML's Election
+            Machines. JS/TS, AWS, NodeJS, React, Vue, Python, Cloudformation.
+          </p>
+        </li>
+        <li>
+          <h3 class="text-xl font-sans flex">
+            <span class="year">2016</span>
+            <a href="https://www.feedbackly.com/">Feedbackly Oy — Junior developer</a>
+          </h3>
+          <p>
+            I was hired to work as part-time duo with another student but since he quit, the thing
+            fell apart and I went to continue my studies full-time. JS, NodeJS, Vue, AngularJS,
+            MongoDB.
+          </p>
+        </li>
+        <li>
+          <h3 class="text-xl font-sans flex">
+            <span class="year">2016</span>
+            <a href="https://www.helsinki.fi/en">University of Helsinki — Junior developer</a>
+          </h3>
+          <p>
+            I was hired to continue working on thesis management software (Grappa) which we built as
+            part of software production course project. JS, React, NodeJS, Postgres, SemanticUI.
+          </p>
+        </li>
+        <li>
+          <h3 class="text-xl font-sans flex">
+            <span class="year">2016-2017</span>
+            <a href="https://www.helsinki.fi/en">University of Helsinki — Course instructor</a>
+          </h3>
+          <p>
+            I was a course instructor for University of Helsinki's database project course a couple
+            of times.
+          </p>
         </li>
       </ul>
     </fieldset>
-    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4 text-sm">
+    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4">
       <legend class="text-base px-1"> Academia </legend>
       <ul>
         <li>
@@ -128,7 +198,8 @@
         <li>
           <h3 class="text-xl font-sans">MSc. of Data Science, University of Helsinki 2018-2021</h3>
           <p class="ml-1">
-            Learned a sufficient amount to know the limits of my knowledge about DS.
+            Learned a sufficient amount to know the limits of my knowledge about DS. Wrote my thesis
+            on clustering and information retrieval.
             <a href="https://helda.helsinki.fi/handle/10138/325709"
               >https://helda.helsinki.fi/handle/10138/325709</a
             >
@@ -148,7 +219,7 @@
         </li>
       </ul>
     </fieldset>
-    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4 text-sm">
+    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4">
       <legend class="text-base px-1"> Projects </legend>
       <ul>
         <li>
@@ -175,16 +246,17 @@
           <h3 class="text-xl font-sans">(untitled)</h3>
           <p>
             I've worked on this Rust synchronization server for one of my other projects but since
-            it's still WIP with rather hacky code, I've felt it's not ready to be published.
+            it's still WIP with rather hacky code, I've kept putting off publishing it. Rust, tokio
+            and setting up infra at Hetzner has been really interesting (and painful) to learn.
           </p>
         </li>
       </ul>
     </fieldset>
-    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4 text-sm">
+    <fieldset class="flex flex-col rounded border-2 px-4 py-2 pb-4 my-4">
       <legend class="text-base px-1"> Hobbies </legend>
       <ul>
         <li>
-          <p>Gym, music, books, writing and programming</p>
+          <p>Gym, music, books, writing, cooking and programming</p>
         </li>
       </ul>
     </fieldset>
@@ -199,7 +271,7 @@
     height: 29.7cm; // A4 is 210 x 297 mm
     width: 21cm;
 
-    @apply font-mono text-sm;
+    @apply font-mono text-xs;
   }
   header {
     display: grid;
@@ -221,7 +293,7 @@
     // }
   }
   a:not(a[class='anchor']) {
-    @apply break-all;
+    // @apply break-all;
     &:hover {
       @apply underline;
     }
