454c98bde8928732266b818af22cd84db8984702
Teemu Koivisto <tkoivisto456@gmail.com>
2024-04-10 13:50:48 +0300

    fix prerender draft pages as well

--- packages/site/src/routes/(nav)/blog/[slug]/+page.server.ts
+++ packages/site/src/routes/(nav)/blog/[slug]/+page.server.ts
@@ -3,6 +3,7 @@ import type * as Kit from '@sveltejs/kit'
 import { parseBlogPosts, type BlogPost } from '$lib/render'
 import * as commentApi from '$lib/api/comments'
 
+import type { EntryGenerator } from './$types'
 import type { Comment } from '@teemukoivisto.xyz/utils'
 
 type RouteParams = {
@@ -28,3 +29,12 @@ export const load: Kit.Load<RouteParams> = async ({ params }) => {
   // }
   return { slug, comments, post: res[1].find(p => p.slug === slug) } as PageData
 }
+
+// Pre-renders pages that are not linked elsewhere (drafts)
+// https://kit.svelte.dev/docs/page-options#entries
+export const entries: EntryGenerator = async () => {
+  const posts = await parseBlogPosts(import.meta.glob('/blog/**/*.md', { as: 'raw', eager: true }))
+  return posts
+}
+
+export const prerender = true
