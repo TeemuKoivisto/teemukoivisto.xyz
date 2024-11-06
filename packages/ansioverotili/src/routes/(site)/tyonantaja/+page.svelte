d8abf06b6bbe0c73803cc975ea5d77bfc1905b88
Teemu Koivisto <tkoivisto456@gmail.com>
2024-11-07 00:24:50 +0200

    add example payment form

--- packages/ansioverotili/src/routes/(site)/tyonantaja/+page.svelte
+++ packages/ansioverotili/src/routes/(site)/tyonantaja/+page.svelte
@@ -4,6 +4,9 @@
 
   import { actions } from '$stores/state'
   import Table from '$components/Table.svelte'
+  import { Button } from '$shadcn/ui/button/index.js'
+  // import { Input } from "$shadcn/ui/input/index.js";
+  // import { Label } from "$shadcn/ui/label/index.js";
 </script>
 
 <h1 class="mt-12 mb-12 font-title text-5xl flex items-center justify-between w-full">
@@ -15,10 +18,42 @@
 
 <div class=" h-full w-full flex items-center">
   <section
-    class="h-[720px] w-[720px] p-16 pt-8 mx-auto bg-white text-container rounded-2xl flex flex-col shadow-xl"
+    class="mb-16 w-[720px] p-16 pt-12 mx-auto bg-white text-container rounded-2xl flex flex-col shadow-xl"
   >
     <div class="flex flex-col gap-4">
-      <div class="font-title text-2xl">Matti Meikäläinen</div>
+      <div class="flex flex-col space-y-1.5 p-0 pb-0">
+        <div class="font-semibold leading-none tracking-tight text-2xl mb-2">Uusi suoritus</div>
+        <p class="text-muted-foreground text-sm">
+          Maksa palkka työntekijälle ilman erillisiä veromaksuja, suoraan ansiotuloverotilille.
+        </p>
+      </div>
+      <div class="space-y-2">
+        <div class="space-y-1">
+          <label
+            for="receiver"
+            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >Palkansaaja</label
+          >
+          <input class="input" id="receiver" value="Matti Meikäläinen FI10 2000 3000 4000 50" />
+        </div>
+        <div class="space-y-1">
+          <label
+            for="salary"
+            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >Palkkasuoritus</label
+          >
+          <input
+            class="input"
+            id="salary"
+            placeholder="Palkka ilman työeläkemaksuja"
+            value="3200"
+          />
+        </div>
+      </div>
+      <div class="flex items-center pt-0">
+        <Button class="bg-blue-500 hover:bg-blue-500/90">Maksa</Button>
+      </div>
+      <h2 class="font-title text-2xl">Matti Meikäläinen</h2>
       <div>FI10 2000 3000 4000 50</div>
       <input
         class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
@@ -36,4 +71,7 @@
 </div>
 
 <style lang="postcss">
+  .input {
+    @apply placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50;
+  }
 </style>
