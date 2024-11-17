1b40ab63e52b3857ed02ebc5eae00c357fddaec0
Teemu Koivisto <tkoivisto456@gmail.com>
2024-11-17 17:23:54 +0200

    enhance description

--- packages/ansioverotili/src/routes/(site)/about/+page.svelte
+++ packages/ansioverotili/src/routes/(site)/about/+page.svelte
@@ -25,16 +25,16 @@
       </p>
       <p>
         Yksi tärkeimpiä valtion tehtäviä on tehdä työnteko helpoksi niin työntekijälle kuin
-        työnantajalle. Suomi on ehkä kehittyneimpiä maita digitalisaation käytössä tässä mutta
-        minusta meillä olisi vielä paljon parannettavaa. Itselleni yksi ärsyttävimmistä asioista
-        työnteossa on ollut <b>verokortti</b>.
+        työnantajalle. Suomi on ehkä yksi kehittyneimpiä maita digitalisaatiossa mutta minusta
+        meillä olisi yhä paljon parannettavaa. Itselleni yksi ärsyttävimmistä asioista työnteossa on
+        ollut <b>verokortti</b>.
       </p>
       <p>
         Verokortin funktio on tarjota yksi kiinteä veroprosentti jonka avulla työnantaja laskee
         työntekijän palkasta verohallinnolle menevän osuuden. Tämän veroprosentin laskenta on
         kuitenkin täyttä ajan ja mielenrauhan haaskausta koska koko prosessi vaatii että <b
           >tiedät</b
-        > kuinka paljon tulet ansaitsemaan vuodessa heti Tammikuusta alkaen.
+        > kuinka paljon tulet ansaitsemaan vuodessa heti tammikuusta alkaen.
       </p>
       <p>
         Jos lasket väärin, kuten usein käy, joudut käsipelin korjaamaan prosentin ja lähettämään
@@ -45,7 +45,7 @@
       </p>
       <hr class="my-4" />
       <p>
-        Pitkään tätä ongelmaa mutustelleena sain yhtäkkiä keksittyä ongelmaan viimein ratkaisun: <u
+        Pitkään tätä ongelmaa mutustelleena tässä eräänä päivänä tuli päähäni ratkaisu: <u
           >luodaan jokaiselle suomalaiselle oma <b>ansiotuloverotili</b>.
         </u>
         Että mikä kysyt? Käytännössä se on uusi henkilökohtainen pankkitili jonka kautta
@@ -53,23 +53,25 @@
         tulot maksetaan palkansaajalle. Applikaatio sen taustalla sitten laskee dynaamisesti veroprosentin
         koko vuoden ansioiden perusteella, siirtäen palkan annetulle veronpalautustilille kuten aiemminkin.
         Työntekijän ei tarvitse huolehtia enää tulojensa arvioimisesta (aivan älytöntä touhua) ja työnantajakin
-        joutuu tekemään enää vain yhden tilisiirron kahden sijaan (palkansaajalle & verovirastoon).
+        joutuu tekemään enää vain yhden tilisiirron kahden sijaan (palkansaajalle & verottajalle).
       </p>
       <p>
-        Tämän idean sain jossain määrin Revolutista jossa käyttäjille luodaan välittömästi aina oma
-        liettualainen pankkitili johon palkan sun muun voi maksaa. Homman juju on koko prosessin
-        vaivattomuus enkä usko että Suomessakaan homman tekninen toteutus olisi suuren työn takana.
-        Tämän lisäksi palvelua pystyisi varmasti kehittämään paljon monipuolisemmaksi. Esimerksi
-        toiminimi-yrittäminen voitaisiin tehdä jokaiselle täysin kitkattomaksi pätkätöiden ja
-        yrittämisen helpottamiseksi, eikä välikäsiä—Ukkoja tai muita—enää tarvittaisi.
+        Tämä idea jossain määrin on kummunnut <a href="https://www.revolut.com/">Revolutista</a>,
+        jossa käyttäjille luodaan välittömästi aina oma liettualainen pankkitili johon palkan sun
+        muun voi maksaa. Homman juju on koko prosessin vaivattomuus, enkä usko että Suomessakaan
+        homman tekninen toteutus olisi suuren työn takana. Tämän lisäksi palvelua pystyisi varmasti
+        kehittämään paljon monipuolisemmaksi. Esimerksi toiminimi-yrittäminen voitaisiin tehdä
+        jokaiselle täysin kitkattomaksi pätkätöiden ja yrittämisen helpottamiseksi, eikä
+        välikäsiä—Ukkoja tai muita—enää tarvittaisi.
       </p>
       <p>
         Tekemällä tästä vielä kunnon rajapinnan, erilaiset palkanpäällisetkin voitaisiin helposti
         liittää palkanmaksuun jotta työntekijälle olisi <b>aina</b> absoluuttisen selvää paljon bruttotuloja
         on hänelle maksettu. Pidemmän päälle haluaisin että tulevaisuudessa Suomessakin puhuttaisiin
         enää vain bruttopalkoista, eläkekuluine kaikkineen, jotta yleinen tietoisuus työnteon hinnasta
-        kehittyisi ja turhat verokiemurat poistuisivat. Pelkästään jo erilliset työntekijän ja työnantajan
-        eläkemaksut ovat täysin turha keksintö, tehty vain poliittikkojen toimesta ihmisten hämäämiseksi.
+        kehittyisi ja turhat verokiemurat poistuisivat. Erilliset työntekijän ja työnantajat eläkemaksut
+        ovat yksi esimerkki, miten järjestelmästä on tehty turhan vaikea ymmärtää tavallisen tallaajan
+        näkökulmasta.
       </p>
       <p>
         <a href="/palkanlaskija" class="underline hover:bg-gray-100">Tässä esimerkki</a>
@@ -79,20 +81,19 @@
         sivulta vuoden 2025 eri maksuista työntekijälle ja työnantajalle. Yritäpä tuosta sitten ymmärtää
         paljon sinulle oikeasti sitä palkkaa maksetaan. Työnantajalle palkanlaskusovellus osaa nämä hoitaa,
         mutta bruttomaksut olisivat <i>niin</i> paljon helpompia jäsentää ja laskea kuin x + eläke +
-        työttömyysvakuutus + kaikenmaailman lounaslippusälät. Ymmärrän kyllä lounasedun hyvät puolet,
-        se on pieni bonus palkan päälle johon kitsainkin työnantaja usein suostuu koska se nähdään palkasta
-        erillisenä maksuna.
+        työttömyysvakuutus + kaikenmaailman lounaslippusälät.
       </p>
       <p>
-        <i>Mutta</i> käytännössä se on täysin abstrakti keksintö, jonka voisi korvata vain palkkoja nostamalla
-        ja ravintola ALV:ia pikkuisen laskemalla. Nykymaailmassa kuitenkin ihmiset yhä enemmän tekevät
-        töitä kotona ja/tai kokkaavat itse, säästäen ja usein syöden terveellisemmin, mitä lounasetu
-        ei kuitenkaan tue. Tai he vain yksinkertaisesti unohtavat vinguttaa lounaskorttia, kasaten järkyttävän
-        saldon tilille ja yrityksen hallinnon hukaten aikaa lounareiden kanssa säätämiseen. Ja se että
-        lounaiden hinnaksi usein laitetaan edun maksimimäärä on jotenkin koomista.
+        Suomessa on ollut paljon puhetta kilpailukyvyn parantamisesta ja kuinka työelämästä ja
+        yrittämisestä voitaisiin tehdä entistä ketterämpää. Minusta paras tapa tähän olisi luoda
+        Suomeen <i>maailman helpoin</i> verojärjestelmä niin palkansaajille kuin yrityksillekin. Yksinkertaistamalla
+        ja tehostamalla prosesseja me luomme hedelmällisemmän ympäristön asioiden tekemiselle, sen sijaan
+        että ne lannistaisivat meitä monimutkaisuudellaan. Minusta ansioverotulotili olisi tähän yksi
+        parannus.
       </p>
-      <p>
+      <p class="flex flex-col">
         <b>Teemu Koivisto {date.toFormat('dd.MM.yyyy')}</b>
+        <i>teemukoivisto.xyz ~АТ~ gmail.com</i>
       </p>
       <p>
         <a
